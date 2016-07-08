f30eb559a0c7ee188b751050f477778754ad2445
jyuan <shi_jy@ctrip.com>
2016-07-08 18:06:13 +0800

    modify sqlcommand.py

--- sqlcommand.py
+++ sqlcommand.py
@@ -1,5 +1,13 @@
+# -*- coding:utf-8 -*-
+
 import re
 import os
+
+class ParamError(Exception):
+    def __init__(self,msg):
+        super(ParamError,self).__init__(msg)
+
+
 def parse_param_config(strconfig):
     sep=os.linesep
     vardict={}
@@ -28,62 +36,15 @@ def parse_param_config(strconfig):
         variable_name=''
     return vardict
 
-
-print parse_param_config(ss)
-
-
-class ParamError(Exception):
-    def __init__(self,msg):
-        super(ParamError,self).__init__(msg)
-
-
-
-
-class MsDnsParser(object):
-    pattern_mssql_dns=r"(?P<servername>\D+\w*)\.db.(?P<env>sh|sh2|sh3|fat|uat|lpt)?(?(env)\.)(?P<domoin>qa.nt.ctripcorp|ctripcorp|ctriptravel){1}\.com,(?P<port>\d+)"
-
+class Command(object):
     def __init__(self):
-        self.pattern=re.compile(MsDnsParser.pattern_mssql_dns,re.I)
-        
-    def isvalid(self,dns):
-        m=self.pattern.match(dns)
-        if m:
-            return True
-        else:
-            return False
-    
-    def parser(self,dns):
-        m=self.pattern.match(dns)
-        if m:
-            return m.groups()
-        else:
-            return None
-    def parser_dict(self,dns):
-        m=self.pattern.match(dns)
-        if m:
-            return m.groupdict()
-        else:
-            return None
-
-def parse_mssql_connection_string(dns):
-    dnsparser=MsDnsParser()
-    dnsobj= dnsparser.parser_dict(dns)
-    #print dnsobj
-    if dnsobj:
-        dns1=p=dns.partition(",")[0]
-        env=dnsobj["env"]
-        port=dnsobj["port"]
-        return (dns1,env,port)
-    else:
-        raise ValueError("Dns format is error:%s" % (dns))
-
-
+        pass
 
 class SqlCommand(Command):
     """
     $sql 必填参数，可执行的sql语句
-    $dns 可选参数，只执行sql的目标服务器，该变量可继承
-    $port 可选参数，当$dns是域名或者IP时，那么一定要提供$port
+    $dns 可选参数，指执行sql的目标服务器，该变量可继承
+    $port 可选参数，当$dns是域名或者IP时，那么一定要提供$port，该变量可继承
     $return 可选参数，临时存储查询结果集时，指定结果集的名称
 
     example:
@@ -92,66 +53,87 @@ class SqlCommand(Command):
         $dns=fltorder.db.ctripcorp.com,55944
         $return=tableinfo
     """
-    options_fix=["$sql","$dns"]
-    options_option=["$return"]
+    options_fix=["$sql"]
+    options_option=["$dns","$port","$return","$db"]
     
     def __init__(self,str_config):
+        self.connect_string=""
+        self.db=""
+        self.has_return_set=False
         self.config_option=parse_param_config(str_config)
-        self._check_config(self.config_option)
-
+        self._check_config()
     def _check_config(self):
-        cf=self.config
-        for option in options_fix:
+        cf=self.config_option
+        print cf
+        for option in SqlCommand.options_fix:
             try:
                 _ov=cf[option]
-                if option=="$dns":
-                    is_mssql,data= self._is_mssql_connect_string(_ov)
-                    if not is_mssql:
-                        if not cf.has_key("$port"):
-                            raise ParamError("lack the parameter $port")
-                    else self._is_dns(_ov) or self._is_ip(_ov):
-                        if "$port" not in cf :
-                    else:
-                        raise ParamError("need this parameter %s" % (option,))
             except KeyError:
-                raise ParamError("need this parameter %s" % (option,))
+                raise ParamError("must provide parameter %s" % (option,))
 
-    def _is_dns(self):
-        pattern_dns=r"(?P<servername>\D+\w*)\.db.(?P<env>sh|sh2|sh3|fat|uat|lpt)?(?(env)\.)(?P<domoin>qa.nt.ctripcorp|ctripcorp|ctriptravel){1}\.com,(?P<port>\d+)"
+        if "$dns" in cf:
+            _ov=cf["$dns"]
+            is_mssql= self._is_mssql_connect_string(_ov)
+            
+            if not is_mssql:
+                print 1
+                if self._is_dns(_ov) or self._is_ip(_ov):
+                    if "$port" not in cf :
+                        raise ParamError("lack the parameter $port")
+                    else:
+                        port=0
+                        try:
+                            port=int(cf["$port"])
+                        except:
+                            raise ParamError("$port must be a integer")
+                        self.connect_string="%s,%d" % (_ov,port)
+                else:
+                    raise ParamError("$dns ( %s ) is error" % (_ov,))
+            else:
+                self.connect_string=_ov
+        if not "$db" in cf:
+            self.db=cf["$db"]="master"
+        if "$return" in cf:
+            self.has_return_set=True
+
+    def _is_dns(self,dns):
+        pattern_dns=r"(?P<servername>\D+\w*)\.db.(?P<env>sh|sh2|sh3|fat|uat|lpt)?(?(env)\.)(?P<domoin>qa.nt.ctripcorp|ctripcorp|ctriptravel){1}\.com"
         pattern=re.compile(pattern_dns,re.I)
         m=pattern.match(dns)
         if m:
             return True
         else:
             return False
-    def _is_ip(self):
+    def _is_ip(self,ip):
         pattern_ip=r"(\d{1,3})\.(\d{1,3}).(\d{1,3})\.(\d{1,3})"
         pattern=re.compile(pattern_ip,re.I)
-        m=pattern.match(dns)
+        m=pattern.match(ip)
         if m:
             return True
         else:
             return False
-    def _is_mssql_connect_string(self):
-        pattern_mssql_str=r"(?P<servername>\D+\w*)\.db.(?P<env>sh|sh2|sh3|fat|uat|lpt)?(?(env)\.)(?P<domoin>qa.nt.ctripcorp|ctripcorp|ctriptravel){1}\.com,(?P<port>\d+)"
+    def _is_mssql_connect_string(self,dns):
+        pattern_mssql_str=r"((?P<servername>\D+\w*)\.db.(?P<env>sh|sh2|sh3|fat|uat|lpt)?(?(env)\.)(?P<domoin>qa.nt.ctripcorp|ctripcorp|ctriptravel){1}\.com,(?P<port>\d+)|(\d{1,3})\.(\d{1,3}).(\d{1,3})\.(\d{1,3}),\d+)"
         pattern=re.compile(pattern_mssql_str,re.I)
         m=pattern.match(dns)
         if m:
             d=m.groupdict()
-            return True,(d["servername"],d["env"],d["domoin"],d["port"])
+            return True#,(d["servername"],d["env"],d["domoin"],d["port"])
         else:
-            return False,(None,)
-    def substitute_sql(self,frame):
-        t=SqlTemplate(self._sql)
+            return False#,(None,)
+    def _substitute_sql(self,frame):
+        cf=self.config_option
+        t=SqlTemplate(cf["$sql"])
         return t.substutite(frame.localVariables)
+
     def run(self,frame):
-        sql=self.substitute_sql(frame)
+        sql=self._substitute_sql(frame)
         if '$' not in sql:
             conn=None
             try:
-                conn=mssql.get_connect(self._host,self._db)
+                conn=mssql.get_connect(self.connect_string,self.db)
                 if self._hasresult:
-                    retset=conn.execute_query_dict(substitute_sql(self._sql))
+                    retset=conn.execute_query_dict(sql)
                     frame.set_local_variable(_result_name,transfer_to_briefobject(retset))
                 else:
                     conn.execute_update(sql)
@@ -162,6 +144,18 @@ class SqlCommand(Command):
         else:
             raise ValueError("sql中有部分变量未替换")
 
+    @property
+    def db(self):
+        return self.db
+
+    @property
+    def connect_string(self):
+        return self.connect_string
+
+    @property
+    def has_return_set(self):
+        return self.has_return_set
+    
     def __str__(self):
         return self.substitute_sql(frame)
 
@@ -170,3 +164,26 @@ class SqlCommand(Command):
 
     def serialize_db(self):
         pass
+
+
+
+
+if __name__ == '__main__':
+
+    ss="""
+$dns=fltorder.db.ctripcorp.com
+$port=55944
+$db='fltorderdb'
+$sql=
+select name 
+from $db.sys.tables(nolock)
+$return=tablesobject
+$a=
+select * from sys.databases
+$b=
+
+select * from cf_services
+
+"""
+    sqlcmd=SqlCommand(ss)
+    print sqlcmd.connect_string
